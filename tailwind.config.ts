64aaca331138bdd4ee81f320c6e6f5d1f4eb3b0b
Oliver Ardizzone <oliver.ardizzone@fuvar.hu>
2023-07-26 04:02:29 +0200

    fix: used themes with tailwind instead

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,14 @@
 import { type Config } from "tailwindcss";
+import defaultTheme from "tailwindcss/defaultTheme";
 
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+		sans: ["'Josefin Sans'", ...defaultTheme.fontFamily.sans]
+	  },
+    },
   },
   plugins: [],
   darkMode: "class",
